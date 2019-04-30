f115a82570b589b62039eeab63767acb7a9f4257
Martin Ewings <martin.ewings@martin.ewings-C02T23THG8WL>
2019-04-30 16:42:05 +0100

    typo

--- spec/acceptance/bash_herd_resolver_spec.rb
+++ spec/acceptance/bash_herd_resolver_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper_acceptance'
 describe 'tasks' do
   it 'testing puppet restarts ' do
-    run_shell('/opt/puppetlabs/bin/puppet config set runinterval 60')
+    run_shell('/opt/puppetlabs/bin/puppet config set runinterval 10')
     result = run_bolt_task('thundering_herd_resolver::bash_herd_resolver')
-    expect(result.first['result']['_output']).to contain('runnning')
+    expect(result.first['result']['_output']).to contain('running')
     expect(result.first['result']['_output']).to contain('stopped')
   end
 end
